76538b39ab82b233297fbf9a02c2844d9f069500
Victor <vamdez123@gmail.com>
2024-09-09 22:18:09 -0300

    create drag behavior

--- crm-front/src/app/Components/StateBox/stateBox.tsx
+++ crm-front/src/app/Components/StateBox/stateBox.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
 
-const StateBox = () => {
+const StateBox = (props: any) => {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: props.id,
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
   return (
-    <div>
-      StateBox
-    </div>
+    <button className='bg-blue-500 h-20 w-20 p-10 flex justify-center items-center' ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
   );
 };
 
 export default StateBox;
+
+//className='bg-blue-500 h-20 w-20 p-10 flex justify-center items-center'
