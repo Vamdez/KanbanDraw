e9ef298c48b55d955d5c7d60aec27d7656c71f4f
Victor <vamdez123@gmail.com>
2024-09-11 20:35:31 -0300

    fix: z-index card in Dragging

--- crm-front/src/app/Components/drageBox/dragBox.tsx
+++ crm-front/src/app/Components/drageBox/dragBox.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import {useDraggable} from '@dnd-kit/core';
 import {CSS} from '@dnd-kit/utilities';
 
-const DragBox = (props: any) => {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
-    id: props.id,
+interface PropsDragBox {
+  id: string;
+  children: React.ReactNode;
+}
+
+const DragBox : React.FC<PropsDragBox> = ({ id, children }) => {
+  const {attributes, listeners, setNodeRef, transform, isDragging} = useDraggable({
+    id: id,
   });
-  const style = {
+
+  const style = transform ? {
     transform: CSS.Translate.toString(transform),
-  };
+    zIndex: isDragging ? 1000 : 'auto',
+    position: 'relative' as 'relative',
+  } : undefined;
 
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      {props.children}
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes} className="w-full mb-2">
+      {children}
     </div>
   );
 };
